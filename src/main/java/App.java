fe40f84db1b5014d25108f7fc4411c9c7e1e565d
jmusau62-gmail <jmusau62@gmail.com>
2021-12-08 17:48:28 +0300

    Added Gui instuctions for user

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class App {
+    public static void main (Strings [] args) {
+        Scanner input =new Scanner (System.in);
+        boolean runningProgram =true;
+        while (runningProgram) {
+            System.out.Println("Hey there , this is the Caesar Cipher, you can choose to encrypt a word, decrypt a word or exit");
+        }
+
+
+
+
+
+
+
+
+    }
+}
