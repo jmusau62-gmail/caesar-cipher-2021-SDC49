f998ee805002ebbdf255264938cb22fb07222539
jmusau62-gmail <jmusau62@gmail.com>
2021-12-08 18:05:54 +0300

    added a test in the ceaser test class

--- src/test/java/CaesarTest.java
+++ src/test/java/CaesarTest.java
@@ -1,2 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class CaesarTest {
-}
+
+    @Test
+    public void runCipher_returnStringPlainText_String(){
+        Caesar testCipher = new Caesar("josh",2);
+        testCipher.getPlainWord();
+        assertEquals("josh", testCipher.getPlainWord());
+    }
