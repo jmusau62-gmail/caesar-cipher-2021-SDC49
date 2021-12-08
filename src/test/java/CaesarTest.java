99afbcd6908bbf177a8c6232daf8dad67953aaa3
jmusau62-gmail <jmusau62@gmail.com>
2021-12-08 18:12:40 +0300

    added 3 more tests in the caesar test java class

--- src/test/java/CaesarTest.java
+++ src/test/java/CaesarTest.java
@@ -9,3 +9,27 @@ public class CaesarTest {
         testCipher.getPlainWord();
         assertEquals("josh", testCipher.getPlainWord());
     }
+
+    @Test
+    public void runCipher_returnIntShift_Integer(){
+        Caesar testCipher2 = new Caesar("wooobababa", 2);
+        testCipher2.getShift();
+        assertEquals(2, testCipher2.getShift());
+
+    }
+
+    @Test
+    public void encrypt_returnEncryptedText_String(){
+        Caesar testCipher3 = new Caesar("josh", 2);
+        testCipher3.encrypt("josh", 2);
+        assertEquals("lquj", testCipher3.encrypt("josh", 2));
+    }
+
+    @Test
+    public void decrypt_returnDecryptedText_String(){
+        Caesar testCipher4 = new Caesar("rko", 2);
+        testCipher4.decrypt("rko", 2);
+        assertEquals("josh", testCipher4.decrypt("lquj",2));
+    }
+
+}
